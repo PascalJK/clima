92ba75838d9ab9904dbd493e3f2358a81ccefca3
Pascal JK <pascual.santos80@gmail.com>
2023-02-06 21:31:16 +0200

    added project defaults.

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({super.key});
+
+  @override
+  State<LoadingScreen> createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  void getLocation() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            //Get the current location
+          },
+          child: const Text('Get Location'),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/utilities/constants.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+const kTempTextStyle = TextStyle(
+  fontFamily: 'Spartan MB',
+  fontSize: 100.0,
+);
+
+const kMessageTextStyle = TextStyle(
+  fontFamily: 'Spartan MB',
+  fontSize: 60.0,
+);
+
+const kButtonTextStyle = TextStyle(
+  fontSize: 30.0,
+  fontFamily: 'Spartan MB',
+);
+
+const kConditionTextStyle = TextStyle(
+  fontSize: 100.0,
+);
+
+const String kCityBackground = 'assets/images/city_background.jpg';
+const String kLocationBackground = 'assets/images/location_background.jpg';
+
+const double kIconSize = 50.0;
