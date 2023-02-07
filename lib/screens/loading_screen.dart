d5cfb68eb4dd5602883f9a9bf9d204327e13fd5b
Pascal JK <pascual.santos80@gmail.com>
2023-02-07 12:30:34 +0200

    - added location permissions to native platforms. - also worked getLocation permission checks.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
@@ -8,7 +9,37 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  void getLocation() {}
+  void getLocation() async {
+    if (!await Geolocator.isLocationServiceEnabled()) {
+      return Future.error('Location services are disabled.');
+    }
+
+    var permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      switch (permission) {
+        case LocationPermission.denied:
+          return Future.error('Location permissions are denied');
+        case LocationPermission.deniedForever:
+          return Future.error(
+              'Location permissions where denied more than once would you like to be navigated to app settings to allow app to use your location?');
+        //TODO Show user an option to go to their app settings and change permissions.
+        default:
+          break;
+      }
+      // if (permission == LocationPermission.denied) {
+      //   // Permissions are denied, next time you could try
+      //   // requesting permissions again (this is also where
+      //   // Android's shouldShowRequestPermissionRationale
+      //   // returned true. According to Android guidelines
+      //   // your App should show an explanatory UI now.
+      //   return Future.error('Location permissions are denied');
+      // }
+    }
+    var position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.medium);
+    print(position.toString());
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +47,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
       body: Center(
         child: ElevatedButton(
           onPressed: () {
-            //Get the current location
+            getLocation();
           },
           child: const Text('Get Location'),
         ),
