6cbf52be23147d72da31ca27b7da10a3624b96d5
Pascal JK <pascual.santos80@gmail.com>
2023-02-08 11:34:04 +0200

    - added spinkit package.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -3,6 +3,7 @@ import 'package:clima/services/location.dart';
 import 'package:clima/services/networking.dart';
 import 'package:clima/utilities/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
@@ -21,6 +22,12 @@ class _LoadingScreenState extends State<LoadingScreen> {
     var uri = Uri.parse(
         "https://api.openweathermap.org/data/2.5/weather?lat=${position.latitude}&lon=${position.longitude}&appid=$kApiKey");
     var data = await NetworkHelper.getData(uri, position);
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => LocationScreen(),
+      ),
+    );
   }
 
   @override
@@ -31,6 +38,13 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const Scaffold(
+      body: Center(
+        child: SpinKitDualRing(
+          color: Colors.white,
+          size: 150,
+        ),
+      ),
+    );
   }
 }
